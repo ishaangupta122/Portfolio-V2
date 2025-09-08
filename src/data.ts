71aba3aa730c240981588a11461d9b952b61f54c
ishaangupta122 <ishaang2209@gmail.com>
2025-09-09 00:58:46 +0530

    Updated UI

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,4 @@
-import { FaRegFileAlt, FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import type { Data } from "./types";
 
